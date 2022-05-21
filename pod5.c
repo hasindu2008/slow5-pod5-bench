afd74cec63555ffcb6580e2603220796d829b2ba
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2022-05-21 14:12:49 +1000

    some comments

--- pod5.c
+++ pod5.c
@@ -1,4 +1,7 @@
-//gcc -Wall -O2 -I pod5_format/include/ -o pod5_pa pod5.c pod5_format/lib64/libpod5_format.a  -lm -lz -lzstd -fopenmp
+//g++-9 -Wall -O2 -I pod5_format/include/ -o pod5_pa pod5.c pod5_format/lib64/libpod5_format.a  -lm -lz -lzstd -fopenmp
+//loads a batch of reads (signal+information needed for pA conversion) from file, process the batch (convert to pA and sum), and write output
+//only the time for loading a batch to memory (Disk I/O + decompression + parsing and filling the memory arrays) is measured
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,15 +18,13 @@ static inline double realtime(void) {
     return tp.tv_sec + tp.tv_usec * 1e-6;
 }
 
-#define TO_PICOAMPS(RAW_VAL,DIGITISATION,OFFSET,RANGE) (((RAW_VAL)+(OFFSET))*((RANGE)/(DIGITISATION)))
-
 typedef struct {
-    char* read_id;                      // the read ID
-    double digitisation;                // the number of quantisation levels - required to convert the signal to pico ampere
-    double offset;                      // offset value - required to convert the signal to pico ampere
-    double scale;                       // range value - required to convert to pico ampere
-    uint64_t len_raw_signal;            // length of the raw signal array
-    int16_t* raw_signal;                // the actual raw signal array
+    char* read_id;
+    double digitisation;
+    double offset;
+    double scale;
+    uint64_t len_raw_signal;
+    int16_t* raw_signal;
 } rec_t;
 
 int main(int argc, char *argv[]){
@@ -33,16 +34,22 @@ int main(int argc, char *argv[]){
         return EXIT_FAILURE;
     }
 
+    // note that batch size of is not really used in POD5 as the concept of a batch seem to be hard coded to the file
+    // need to verify if there is a way
     int batch_size = atoi(argv[3]);
+
     int num_thread = atoi(argv[2]);
     omp_set_num_threads(num_thread);
 
-
     double tot_time = 0;
-    double t0 = realtime();
+    double t0 = 0 ;
+
+    /**** Initialisation and opening of the file ***/
+    t0 = realtime();
+
     pod5_init();
 
-    // Open the file ready for walking:
+    // Open the file
     Pod5FileReader_t* file = pod5_open_combined_file(argv[1]);
 
     if (!file) {
@@ -51,19 +58,19 @@ int main(int argc, char *argv[]){
        exit(EXIT_FAILURE);
     }
 
-
     size_t batch_count = 0;
     if (pod5_get_read_batch_count(&batch_count, file) != POD5_OK) {
         fprintf(stderr, "Failed to query batch count: %s\n", pod5_get_error_string());
     }
     tot_time += realtime() - t0;
-
+    /**** End of init ***/
 
     int read_count = 0;
 
-
+    //iterate through batches in the file
     for (size_t batch_index = 0; batch_index < batch_count; ++batch_index) {
 
+        /**** Fetching a batch (disk loading, decompression, parsing in to memory arrays) ***/
         double t0 = realtime();
 
         Pod5ReadRecordBatch_t* batch = NULL;
@@ -76,9 +83,9 @@ int main(int argc, char *argv[]){
             fprintf(stderr,"Failed to get batch row count\n");
         }
 
-        //parsing?
         rec_t *rec = (rec_t*)malloc(batch_row_count * sizeof(rec_t));
 
+        // need to find out of this part can be multi-threaded, and if so the best way
         for (size_t row = 0; row < batch_row_count; ++row) {
             uint8_t read_id[16];
             int16_t pore = 0;
@@ -99,7 +106,6 @@ int main(int argc, char *argv[]){
             char read_id_tmp[37];
             pod5_error_t err = pod5_format_read_id(read_id, read_id_tmp);
 
-
             // Now read out the calibration params:
             CalibrationDictData_t *calib_data = NULL;
             if (pod5_get_calibration(batch, calibration_idx, &calib_data) != POD5_OK) {
@@ -109,13 +115,12 @@ int main(int argc, char *argv[]){
             // Find the absolute indices of the signal rows in the signal table
             uint64_t *signal_rows_indices= (uint64_t*) malloc(signal_row_count * sizeof(uint64_t));
 
-
-
             if (pod5_get_signal_row_indices(batch, row, signal_row_count,
                                            signal_rows_indices) != POD5_OK) {
                 fprintf(stderr,"Failed to get read %ld; signal row indices: %s\n", row, pod5_get_error_string());
             }
 
+            // cannot get to work this in C, So using C++
             // Find the locations of each row in signal batches:
             //SignalRowInfo_t *signal_rows = (SignalRowInfo_t *)malloc(sizeof(SignalRowInfo_t)*signal_row_count);
             std::vector<SignalRowInfo_t *> signal_rows(signal_row_count);
@@ -154,12 +159,12 @@ int main(int argc, char *argv[]){
             pod5_free_signal_row_info(signal_row_count, signal_rows.data());
 
             free(signal_rows_indices);
-            //free(signal_rows);
 
         }
         tot_time += realtime() - t0;
+        /**** Batch fetched ***/
 
-        //process
+        //process and print (time not measured as we want to compare to the time it takes to read the file)
         double *sums = (double*)malloc(batch_row_count * sizeof(double));
         #pragma omp parallel for
         for(int i=0;i<batch_row_count;i++){
@@ -175,7 +180,7 @@ int main(int argc, char *argv[]){
         }
         fprintf(stderr,"batch printed with %ld reads\n",batch_row_count);
 
-        //free
+        /**** Deinit ***/
         t0 = realtime();
         if (pod5_free_read_batch(batch) != POD5_OK) {
             fprintf(stderr,"Failed to release batch\n");
@@ -187,6 +192,7 @@ int main(int argc, char *argv[]){
         }
         free(rec);
         tot_time += realtime() - t0;
+        /**** End of Deinit***/
 
     }
 
