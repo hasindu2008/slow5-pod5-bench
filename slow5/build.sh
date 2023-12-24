5770f8ca628d4d64abd6f7f69b3c47865128b399
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2023-12-25 00:56:43 +1100

    update build script

--- slow5/build.sh
+++ slow5/build.sh
@@ -4,6 +4,6 @@ set -x
 set -e
 
 test -e zstd/lib/libzstd.a || ./install-zstd.sh
-cd slow5lib && make clean && make -j zstd=1 slow5_mt=1 && cd ..
-gcc -Wall -O2 -g -I slow5lib/include/ -o slow5_sequential sequential.c slow5lib/lib/libslow5.a zstd/lib/libzstd.a -lm -lz -lzstd -fopenmp
-gcc -Wall -O2 -g -I slow5lib/include/ -o slow5_convert_to_pa_rand slow5_convert_to_pa_rand.c slow5lib/lib/libslow5.a zstd/lib/libzstd.a -lm -lz -lzstd -fopenmp
+cd slow5lib && make clean && make -j slow5_mt=1 zstd_local=../zstd/lib/  && cd ..
+gcc -Wall -O2 -g -I slow5lib/include/ -o slow5_sequential sequential.c slow5lib/lib/libslow5.a zstd/lib/libzstd.a  -lm -lz -fopenmp
+gcc -Wall -O2 -g -I slow5lib/include/ -o slow5_convert_to_pa_rand slow5_convert_to_pa_rand.c slow5lib/lib/libslow5.a zstd/lib/libzstd.a -lm -lz -fopenmp
