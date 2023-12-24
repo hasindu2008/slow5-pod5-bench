71d00811eab1cb49f28a6dce7ff06d1ce9081863
hiruna72 <hiruna72@gmail.com>
2023-12-24 21:33:41 +1100

    pod5 serial uses cxxpool for processing as well

--- pod5/build.sh
+++ pod5/build.sh
@@ -3,5 +3,6 @@
 set -x
 set -e
 
-g++ -Wall -O2 -g -I pod5_format/include -I cxxpool/src -o pod5_convert_to_pa pod5_convert_to_pa.cpp pod5_format/lib/libpod5_format.so -lm -lz -lzstd -fopenmp
+#g++ -Wall -O2 -g -I pod5_format/include -I cxxpool/src -o pod5_convert_to_pa pod5_convert_to_pa.cpp pod5_format/lib/libpod5_format.so -lm -lz -lzstd -fopenmp
+g++ -Wall -O2 -g -I pod5_format/include -I cxxpool/src -o pod5_convert_to_pa pod5_convert_to_pa.cpp pod5_format/lib/libpod5_format.so -lm -lz -lzstd -lpthread
 #g++ -Wall -O2 -g -I pod5_format/include -I cxxpool/src -o pod5_convert_to_pa_rand pod5_convert_to_pa_rand.cpp pod5_format/lib/libpod5_format.so -lm -lz -lzstd -fopenmp
