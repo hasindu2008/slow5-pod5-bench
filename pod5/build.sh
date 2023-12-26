c707b7f695f51119c1c9447f23542ed2a416460e
hiruna72 <hiruna72@gmail.com>
2023-12-26 21:58:39 +1100

    correct spelling

--- pod5/build.sh
+++ pod5/build.sh
@@ -3,4 +3,4 @@
 set -x
 set -e
 
-g++ -Wall -O2 -g -I pod5_format/include -I cxxpool/src -o pod5_sequntial sequntial.cpp pod5_format/lib/libpod5_format.so -lm -lz -lzstd -fopenmp -lpthread
+g++ -Wall -O2 -g -I pod5_format/include -I cxxpool/src -o pod5_sequential sequential.cpp pod5_format/lib/libpod5_format.so -lm -lz -lzstd -fopenmp -lpthread
