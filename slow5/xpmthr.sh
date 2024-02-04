7906a0ec4bc9c9a480c0d3444c952019577461ed
Sasha Jenner <me@sjenner.com>
2024-02-04 10:17:17 +0100

    fixing taskset

--- slow5/xpmthr.sh
+++ slow5/xpmthr.sh
@@ -5,9 +5,7 @@ USAGE="usage: $0 <slow5> <ids>"
 
 . ./cf.sh
 
-THREADS='1 2 4 8 16'
 BATCH_SZ=1024
-TASKSET_CPUS=1
 
 die()
 {
@@ -36,7 +34,10 @@ clfs()
 
 bench()
 {
-	/usr/bin/time -v taskset -c "$TASKSET_CPUS" $@
+	nthr=$1
+	lastproc=$(echo "$nthr-1"| bc)
+	shift
+	/usr/bin/time -v taskset -a -c 0-"$lastproc" $@
 }
 
 diffchk()
@@ -48,6 +49,21 @@ diffchk()
 		diff -q "$1" "$2" || die "'$1' and '$2' differ"
 	fi
 }
+
+seqgeom()
+{
+	ret=
+	i=$1
+	while [ "$i" -le "$3" ]
+	do
+		ret="$ret $i"
+		i=$(echo "$i * $2" | bc)
+	done
+	echo "$ret"
+}
+
+nproc=$(nproc --all)
+THREADS=$(seqgeom 1 2 "$nproc") # geometric sequence
 
 if ! [ -e "$SLOW5".idx ]
 then
@@ -62,7 +78,7 @@ do
 	# random
 	stderr=$SLOW5.xpmthr"$i"_"$RAND"_stderr
 	clfs
-	bench "./$RAND" "$SLOW5" "$IDS" "$i" "$BATCH_SZ" \
+	bench "$i" "./$RAND" "$SLOW5" "$IDS" "$i" "$BATCH_SZ" \
 		> "$TMP" 2> "$stderr" \
 		|| die "$RAND failed for $i threads"
 	diffchk "$RAND_OUT" "$TMP"
@@ -70,7 +86,7 @@ do
 	# sequential
 	stderr=$SLOW5.xpmthr"$i"_"$SEQ"_stderr
 	clfs
-	bench "./$SEQ" "$SLOW5" "$i" "$BATCH_SZ" \
+	bench "$i" "./$SEQ" "$SLOW5" "$i" "$BATCH_SZ" \
 		> "$TMP" 2> "$stderr" \
 		|| die "$SEQ failed for $i threads"
 	diffchk "$SEQ_OUT" "$TMP"
@@ -80,7 +96,7 @@ do
 		# sequential cxxpool
 		stderr=$SLOW5.xpmthr"$i"_"$SEQ_CXX"_stderr
 		clfs
-		bench "./$SEQ_CXX" "$SLOW5" "$i" "$BATCH_SZ" \
+		bench "$i" "./$SEQ_CXX" "$SLOW5" "$i" "$BATCH_SZ" \
 			> "$TMP" 2> "$stderr" \
 			|| die "$SEQ_CXX failed for $i threads"
 		diffchk "$SEQ_CXX_OUT" "$TMP"
