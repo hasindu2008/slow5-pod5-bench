a7c257bc75fcf0dda98519f8f7888d4e4118a747
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-06-24 23:18:34 +1000

    je

--- slow5/build.sh
+++ slow5/build.sh
@@ -7,5 +7,5 @@ test -e zstd/lib/libzstd.a || ./install-zstd.sh
 export CC=gcc-10
 export CXX=g++-10
 cd slow5lib && make clean && make -j CC=$CC slow5_mt=1 zstd_local=../zstd/lib/  && cd ..
-gcc -Wall -O3 -g -I slow5lib/include/ -o slow5_sequential sequential.c slow5lib/lib/libslow5.a zstd/lib/libzstd.a  -lm -lz -fopenmp
-gcc -Wall -O3 -g -I slow5lib/include/ -o slow5_random random.c slow5lib/lib/libslow5.a zstd/lib/libzstd.a -lm -lz -fopenmp
+$CC -Wall -O3 -g -I slow5lib/include/ -o slow5_sequential sequential.c slow5lib/lib/libslow5.a zstd/lib/libzstd.a  -lm -lz -fopenmp
+$CC -Wall -O3 -g -I slow5lib/include/ -o slow5_random random.c slow5lib/lib/libslow5.a zstd/lib/libzstd.a -lm -lz -fopenmp
