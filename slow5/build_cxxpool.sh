2946f9c2ecf4fc4713ea3cf81a73f68e6187209c
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-06-24 23:09:19 +1000

    update gcc, zstd and jemalloc

--- slow5/build_cxxpool.sh
+++ slow5/build_cxxpool.sh
@@ -6,5 +6,5 @@ set -e
 test -e zstd/lib/libzstd.a || ./install-zstd.sh
 export CC=gcc-10
 export CXX=g++-10
-cd slow5lib && make clean && make -j slow5_mt=1 zstd_local=../zstd/lib/  && cd ..
-g++ -Wall -O3 -g -I slow5lib/include/ -I ../pod5/cxxpool/src -o slow5_sequential_cxxpool sequential_cxxpool.cpp slow5lib/lib/libslow5.a zstd/lib/libzstd.a -lm -lz -fopenmp -lpthread
+cd slow5lib && make clean && make -j CC=$CC slow5_mt=1 zstd_local=../zstd/lib/  && cd ..
+$CXX -Wall -O3 -g -I slow5lib/include/ -I ../pod5/cxxpool/src -o slow5_sequential_cxxpool sequential_cxxpool.cpp slow5lib/lib/libslow5.a zstd/lib/libzstd.a -lm -lz -fopenmp -lpthread
