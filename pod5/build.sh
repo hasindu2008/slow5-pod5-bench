442c81bf3828387a37c2f203d2bd5d529a4e1a14
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-06-28 00:50:44 +1000

    cleanup

--- pod5/build.sh
+++ pod5/build.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-set -x
-set -e
+die() {
+    echo "$1" 1>&2
+    exit 1
+}
 
 export CC=gcc-10
 export CXX=g++-10
 
-$CXX -Wall -O3 -g -I pod5_format/include -I cxxpool/src -o pod5_sequential sequential.cpp pod5_format/lib/libpod5_format.so -lm -lz -lzstd -fopenmp -lpthread
-$CXX -Wall -O3 -g -I pod5_format/include -I cxxpool/src -o pod5_random random.cpp pod5_format/lib/libpod5_format.so -lm -lz -lzstd -fopenmp -lpthread
+$CC --version || die "gcc-10 not found"
+$CXX --version || die "g++-10 not found"
+
+test -e pod5_format/lib/libpod5_format.so || die "pod5_format not found"
+
+$CXX -Wall -O3 -g -I pod5_format/include -I cxxpool/src -o pod5_sequential sequential.cpp pod5_format/lib/libpod5_format.so -lm -lz -lzstd -fopenmp -lpthread || die "Failed to build pod5_sequential"
+$CXX -Wall -O3 -g -I pod5_format/include -I cxxpool/src -o pod5_random random.cpp pod5_format/lib/libpod5_format.so -lm -lz -lzstd -fopenmp -lpthread || die "Failed to build pod5_random"
