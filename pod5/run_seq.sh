3ecafa7c2199ed8582d292bd42ceec5cf726b0f3
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-06-28 23:24:31 +1000

    pod5 script

---
+++ pod5/run_seq.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# run a benchmark experiment
+# specify the number of threads and batch size
+
+USAGE="usage: $0 <pod5> <thr> <batch>"
+
+
+die()
+{
+	echo "$1" 1>&2
+	exit 1
+}
+
+if [ $# -ne 3 ]
+then
+	die "$USAGE"
+fi
+
+POD5=$1
+T=$2
+B=$3
+LAST_PROC=$(echo "$T-1" | bc)
+SEQ_CXX=pod5_sequential
+
+test -e "$POD5" || die "$POD5 does not exist"
+
+clean_fscache || die "clean_fscache failed"
+
+test -x "$SEQ_CXX" || die "$SEQ_CXX does not exist"
+/usr/bin/time -v taskset -a -c 0-"$LAST_PROC" "./$SEQ_CXX" "$POD5" "$T" > /dev/null
+
