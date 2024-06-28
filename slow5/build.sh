04d75325074fc6bce7c79c51e08f2f5b8f527ef0
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-06-29 00:42:21 +1000

    fix include

--- slow5/build.sh
+++ slow5/build.sh
@@ -15,7 +15,7 @@ $CC --version || die "gcc-10 not found"
 $CXX --version || die "g++-10 not found"
 
 cd slow5lib && make clean
-make -j CC=$CC slow5_mt=1 zstd_local=zstd/include/   || die "Building slow5lib failed"
+make -j CC=$CC slow5_mt=1 zstd_local=zstd/lib/   || die "Building slow5lib failed"
 cd ..
 
 $CC -Wall -O3 -g -I slow5lib/include/ -o slow5_sequential sequential.c slow5lib/lib/libslow5.a zstd/lib/libzstd.a  -lm -lz -fopenmp || die "compilation failed"
