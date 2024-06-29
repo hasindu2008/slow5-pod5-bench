edc0c838ac07a4863cc2e6fa11fa2214a65d34ad
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-06-29 12:58:41 +1000

    scripttt

---
+++ slow5/manualbench/system/gtgpu-ssd.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+
+USAGE="usage: $0 <slow5> <thr> <batch> <c/cxx>"
+
+
+die()
+{
+	echo "$1" 1>&2
+	exit 1
+}
+
+cd /data/hasindu/hasindu2008.git/slow5-pod5-bench/slow5 || die "cd fail"
+cp /home/hasindu/scratch/hg2_prom_lsk114_5khz/PGXXXX230339_reads_zstd-sv16-zd.blow5 /data/tmp/PGXXXX230339_reads_zstd-sv16-zd.blow5 || die "cp fail"
+
+echo "BLOW5 C"
+for i in $(seq 1 5); do
+	echo "Iteration $i"
+	./run_seq.sh /data/tmp/PGXXXX230339_reads_zstd-sv16-zd.blow5  20 1000 c &> gtgpu-ssd_PGXXXX230339_reads_zstd-sv16-zd_20_1000_c_1.log
+done
+
+echo "BLOW5 CXX"
+for i in $(seq 1 5); do
+echo "Iteration $i"
+	./run_seq.sh /data/tmp/PGXXXX230339_reads_zstd-sv16-zd.blow5  20 1000 cxx &> gtgpu-ssd_PGXXXX230339_reads_zstd-sv16-zd_20_1000_cxx_1.log
+done
+
+rm /data/tmp/PGXXXX230339_reads_zstd-sv16-zd.blow5 || die "rm fail"
+
