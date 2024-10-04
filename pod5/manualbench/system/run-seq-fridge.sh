ad7a291d6681a1ad541afb3be00df8047b3f731c
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-10-04 19:03:59 +1000

    generalise pod5 scripts

--- pod5/manualbench/system/run-seq-fridge.sh
+++ pod5/manualbench/system/run-seq-fridge.sh
@@ -4,6 +4,6 @@
 export MACHINE=fridge-ssd
 export FILE=PGXXXX230339
 export SOURCE_DIR=/home/hasindu/slow5-pod5-bench/pod5
-export POD5=PGXXXX230339_reads.pod5
+export POD5=/data3/tmp/PGXXXX230339_reads.pod5
 
 ./manualbench/system/linux-seq.sh
