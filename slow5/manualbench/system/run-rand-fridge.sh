273f19bd7448e90e38471262884c84e40691035d
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-10-05 11:41:37 +1000

    fridge rand

--- slow5/manualbench/system/run-rand-fridge.sh
+++ slow5/manualbench/system/run-rand-fridge.sh
@@ -2,10 +2,10 @@
 
 # edit the following
 # edit the following
-MACHINE=fridge-ssd
-FILE=PGXXXX230339
-SOURCE_DIR=/data/hasindu/hasindu2008.git/slow5-pod5-bench/slow5
-BLOW5=/data3/tmp/PGXXXX230339_reads_zstd-sv16-zd.blow5
-LIST=/data3/tmp/500k.list
+export MACHINE=fridge-ssd
+export FILE=PGXXXX230339
+export SOURCE_DIR=/data/hasindu/hasindu2008.git/slow5-pod5-bench/slow5
+export BLOW5=/data3/tmp/PGXXXX230339_reads_zstd-sv16-zd.blow5
+export LIST=/data3/tmp/500k.list
 
 ./manualbench/system/linux-rand.sh
