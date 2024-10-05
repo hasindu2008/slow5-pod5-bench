6d4894078f89a9e48185def361c5bb5dc92c5940
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-10-05 11:54:11 +1000

    nfs

---
+++ slow5/manualbench/system/run-seq-gtgpu-nfs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# edit the following
+export MACHINE=gtgpu-nfs
+export FILE=PGXXXX230339
+export SOURCE_DIR=/data/hasindu/hasindu2008.git/slow5-pod5-bench/slow5
+export BLOW5=/home/hasindu/scratch/hg2_prom_lsk114_5khz/PGXXXX230339_reads_zstd-sv16-zd.blow5
+
+./manualbench/system/linux-seq.sh
