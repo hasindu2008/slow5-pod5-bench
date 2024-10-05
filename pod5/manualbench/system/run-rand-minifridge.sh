d027d5d6d6984cd56eb085175f0f6eb6384f114b
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-10-05 14:00:10 +1000

    minifridge pod5

---
+++ pod5/manualbench/system/run-rand-minifridge.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# edit the following
+export MACHINE=fridge-ssd
+export FILE=PGXXXX230339
+export SOURCE_DIR=/home/hasindu/slow5-pod5-bench/pod5
+export POD5=/data2/tmp/PGXXXX230339_reads.pod5
+export LIST=/data2/tmp/500k.list
+
+./manualbench/system/linux-rand.sh
