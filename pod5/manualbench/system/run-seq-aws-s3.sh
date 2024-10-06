79a796fb5f4110873fa5f3869860d34ff84a05b6
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-10-06 14:33:04 +1100

    aws pod5

--- pod5/manualbench/system/run-seq-aws-s3.sh
+++ pod5/manualbench/system/run-seq-aws-s3.sh
@@ -4,7 +4,7 @@
 export MACHINE=aws-s3
 export FILE=PGXXXX230339
 export SOURCE_DIR=~/slow5-pod5-bench/pod5
-export BLOW5=~/s3/PGXXXX230339_reads.pod5
+export POD5=~/s3/PGXXXX230339_reads.pod5
 
 ./manualbench/system/linux-seq.sh
 sudo poweroff
