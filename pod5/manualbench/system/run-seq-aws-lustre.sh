3b86a753990f5d97dddcb0a932cc3dcb149a97de
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-10-08 18:00:53 +1100

    pod5 seq

---
+++ pod5/manualbench/system/run-seq-aws-lustre.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# edit the following
+export MACHINE=aws-lustre
+export FILE=PGXXXX230339
+export SOURCE_DIR=~/slow5-pod5-bench/pod5
+export POD5=/fsx/data/PGXXXX230339_reads.pod5
+
+./manualbench/system/linux-seq.sh
+sudo poweroff
