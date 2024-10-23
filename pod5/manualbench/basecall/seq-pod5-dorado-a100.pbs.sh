ac45db2fe3b3fb83d9625de1578457cd829c4724
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-10-24 01:06:52 +1100

    add basecall scripts

--- pod5/manualbench/basecall/seq-pod5-dorado-a100.pbs.sh
+++ pod5/manualbench/basecall/seq-pod5-dorado-a100.pbs.sh
@@ -5,7 +5,7 @@
 #PBS -l ncpus=128
 #PBS -l ngpus=8
 #PBS -l mem=384GB
-#PBS -l walltime=1:00:00
+#PBS -l walltime=3:00:00
 #PBS -l wd
 #PBS -l storage=gdata/if89+scratch/ox63+gdata/ox63
 
@@ -26,7 +26,6 @@ usage() {
 	exit 1
 }
 
-module load /g/data/if89/apps/modulefiles/slow5-dorado/0.3.4
 num_threads=${PBS_NCPUS}
 
 # terminate script
@@ -36,11 +35,11 @@ die() {
 	exit 1
 }
 
-MERGED_SLOW5=/g/data/ox63/hasindu/slow5-pod5-bench/data/PGXXXX230339_reads_zstd-svb-zd.blow5
-TMP_FASTQ=/scratch/ox63/hg1112/slow5-a100.fastq
+MERGED_POD5=/g/data/ox63/hasindu/slow5-pod5-bench/data/
+TMP_FASTQ=/scratch/ox63/hg1112/pod5-a100.fastq
 
 clean_fscache || die "clean_fscache failed"
 
-/usr/bin/time -v  slow5-dorado basecaller ${MODEL} ${MERGED_SLOW5} --emit-fastq --slow5_threads 8 --slow5_batchsize 1000  -x cuda:all  > ${TMP_FASTQ} 2> seq_slow5_gadi_PGXXXX230339_dorado034_8_1000_8a100_hac.log || die "basecalling failed"
+/usr/bin/time -v  /g/data/ox63/hasindu/dorados/dorado-0.3.4/bin/dorado basecaller ${MODEL} ${MERGED_POD5} --emit-fastq  -x cuda:all  > ${TMP_FASTQ} 2> seq_pod5_gadi_PGXXXX230339_dorado034_8_1000_8a100_hac.log || die "basecalling failed"
 
 echo "basecalling success"
